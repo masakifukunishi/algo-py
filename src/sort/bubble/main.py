7b30803edbf85d81b3e6fa5612025ca6780a8e31
MasakiFukunishi <fmsknn@gmail.com>
2022-07-14 23:29:50 +0900

    first commit

---
+++ src/sort/bubble/main.py
@@ -0,0 +1,11 @@
+from typing import List
+def bubble_sort(numbers: List[int]) -> List[int]:
+    len_numbers = len(numbers)
+    for i in range(len_numbers):
+        for j in range(len_numbers - 1 - i):
+            if numbers[j] > numbers[j+1]:
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+    return numbers
+
+if __name__ == '__main__':
+    print(bubble_sort([5, 2, 3, 4]))
