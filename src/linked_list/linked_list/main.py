53b07a036be765250aed8b30ba3d763da0e3f9bb
MasakiFukunishi <fmsknn@gmail.com>
2022-07-17 12:35:20 +0900

    added linked list rec

--- src/linked_list/linked_list/main.py
+++ src/linked_list/linked_list/main.py
@@ -52,12 +52,35 @@ class LinkedList(object):
         previous_node.next = current_node.next
         current_node = None
 
+    def reverse_iterative(self) -> None:
+        previous_node = None
+        current_node = self.head
+        while current_node:
+            next_node = current_node.next
+            current_node.next = previous_node
+            previous_node = current_node
+            current_node = next_node
+
+        self.head = previous_node 
+
+    def reverse_recursive(self) -> None:
+        def _reverse_recursive(current_node: Node, previous_node: Node) -> Node:
+            if not current_node:
+                return previous_node
+
+            next_node = current_node.next
+            current_node.next = previous_node
+            previous_node = current_node
+            current_node = next_node
+            return _reverse_recursive(current_node, previous_node)
+        
+        self.head = _reverse_recursive(self.head, None)
 
 if __name__ == "__main__":
     l = LinkedList()
     l.append(1)
     l.append(2)
-    l.append(4)
+    l.append(3)
     l.print()
-    l.remove(2)
+    l.reverse_recursive()
     l.print()
