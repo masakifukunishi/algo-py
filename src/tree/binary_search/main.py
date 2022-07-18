568635d5c83308e5d8824eb5a525c138816ce6b5
MasakiFukunishi <fmsknn@gmail.com>
2022-07-18 14:43:02 +0900

    added inorder and search to binary serach tree

--- src/tree/binary_search/main.py
+++ src/tree/binary_search/main.py
@@ -16,9 +16,34 @@ def insert(node: Node, value: int) -> Node:
     return node
 
 
+def inorder(node: Node) -> None:
+    # Left, Root, Right
+    if node is not None:
+        inorder(node.left)
+        print(node.value)
+        inorder(node.right)
+
+
+def search(node: Node, value: int) -> bool:
+    if node is None:
+        return False
+
+    if node.value == value:
+        return True
+    elif node.value > value:
+        return search(node.left, value)
+    elif node.value < value:
+        return search(node.right, value)
+
+
 if __name__ == '__main__':
     root = None
     root = insert(root, 3)
     root = insert(root, 6)
     root = insert(root, 5)
-    print(root.value)
+    root = insert(root, 7)
+    root = insert(root, 1)
+    root = insert(root, 10)
+    root = insert(root, 2)
+    # inorder(root)
+    print(search(root, 9))
